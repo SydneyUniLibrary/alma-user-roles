22c35017f00b8e988a1269217f9634816ab1a41d
Jim Nicholls <jim.nicholls@sydney.edu.au>
2019-01-04 12:09:47 +1100

    Verify user roles. but --ignore-extra not implemented.

---
+++ user_roles/verify_user_roles.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+import json
+import logging
+import typing
+
+import alma
+
+
+logger = logging.getLogger('user_roles.verify_user_roles')
+
+
+def verify_user_roles(alma_client: alma.ApiClient, user_id_list: typing.List[str], template_file,
+                      include_inactive: bool = False, ignore_extra: bool = False):
+    template = json.load(template_file)
+    logger.debug('Template: %r', template)
+    if ignore_extra:
+        # TODO: Implement ignore_extra
+        logger.warning('--ignore-extra has not been implemented yet')
+    for user_id in user_id_list:
+        logger.debug('Getting roles of user %s', user_id)
+        user = alma_client.get_user_details(user_id)
+        primary_id = user['primary_id']
+        logger.debug('Got user %r', primary_id)
+        user_role_list = [
+            user_role
+            for user_role in user.get('user_role') or []
+            if include_inactive or alma.is_user_role_active(user_role)
+        ]
+        if user_role_list == template:
+            logger.debug('%s does match the template', user_id)
+        else:
+            print(f'{primary_id} does not match the template')
