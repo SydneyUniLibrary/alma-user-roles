074af2beb62a527c1a0e247b40a5464703ce5f86
Jim Nicholls <jim.nicholls@sydney.edu.au>
2019-06-14 12:21:53 +1000

    Add swagger_client from the downloaded sdk into alma_sdk.

---
+++ alma_sdk/api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from alma_sdk.api.deposits_api import DepositsApi
+from alma_sdk.api.fines_and_fees_api import FinesAndFeesApi
+from alma_sdk.api.leganto_notifications_api import LegantoNotificationsApi
+from alma_sdk.api.loans_api import LoansApi
+from alma_sdk.api.purchase_requests_api import PurchaseRequestsApi
+from alma_sdk.api.requests_api import RequestsApi
+from alma_sdk.api.test_api import TestApi
+from alma_sdk.api.users_api import UsersApi
